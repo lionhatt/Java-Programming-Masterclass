39089d44d8792f462de66ee4346ce504977cd09e
lionhatt <gavindxy@gmail.com>
2021-08-05 21:06:20 +1000

    char and boolean

---
+++ CharAndBoolean/src/CharAndBoolean.java
@@ -0,0 +1,11 @@
+public class CharAndBoolean {
+    public static void main(String[] args) throws Exception {
+
+        char MyChar = 'D';
+        char MyUnicodeChar = '\u0044';
+        System.out.println(MyChar);
+        System.out.println(MyUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+    }
+}
