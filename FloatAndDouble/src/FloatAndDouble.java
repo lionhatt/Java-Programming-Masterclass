9aee4234090e4913d3588d7dcc5409cdf68ce12b
lionhatt <gavindxy@gmail.com>
2021-08-05 20:50:26 +1000

    floating point precision

--- FloatAndDouble/src/FloatAndDouble.java
+++ FloatAndDouble/src/FloatAndDouble.java
@@ -10,9 +10,21 @@ public class FloatAndDouble {
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5.25d;
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.00 / 3.00;
+        System.out.println("MyIntValue= " + myIntValue);
+        System.out.println("MyFloatValue= " + myFloatValue);
+        System.out.println("MyDoubleValue= " + myDoubleValue);
+
+        double numberOfPound = 200d;
+        double convertedKilograms = numberOfPound * 0.45359237d;
+        System.out.println("Converted kilograms= " + convertedKilograms);
+
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;
+        System.out.println(pi);
+        System.out.println(anotherNumber);
 
     }
 }
