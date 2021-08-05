fb9784d9bdd55d17deb6f67357a3d5d27e5be656
lionhatt <gavindxy@gmail.com>
2021-08-05 19:56:29 +1000

    primitive types

--- ByteShortIntLong/src/App.java
+++ ByteShortIntLong/src/App.java
@@ -1,5 +1,15 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimun Value = " + myMinIntValue);
+        System.out.println("Integer Maximun Value = " + myMaxIntValue);
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+        System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+
+        int myMaxIntTest = 2_147_483_647;
     }
 }
