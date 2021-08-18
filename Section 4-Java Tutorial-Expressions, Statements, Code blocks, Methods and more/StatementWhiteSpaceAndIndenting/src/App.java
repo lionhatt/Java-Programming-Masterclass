a398c1e04b8d82bdb6daa2faaeba6e48b2d65703
lionhatt <gavindxy@gmail.com>
2021-08-18 19:52:56 +1000

    Statement, White space and indenting

---
+++ Section 4-Java Tutorial-Expressions, Statements, Code blocks, Methods and more/StatementWhiteSpaceAndIndenting/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int myVariable=50;
+        myVariable++;
+        myVariable--;
+        System.out.println("Thi is a test");
+
+        System.out.print("THis is" + 
+                " another" + 
+                " still more.");
+    }
+}
