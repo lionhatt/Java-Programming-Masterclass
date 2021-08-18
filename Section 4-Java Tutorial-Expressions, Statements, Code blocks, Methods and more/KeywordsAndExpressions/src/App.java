22be3d5185ba4c944d7b0bbc366ac85b13c8251c
lionhatt <gavindxy@gmail.com>
2021-08-18 19:41:27 +1000

    keywords and expressions

---
+++ Section 4-Java Tutorial-Expressions, Statements, Code blocks, Methods and more/KeywordsAndExpressions/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore == 50) {
+            System.out.print("This is an expression");
+        }
+
+        int score = 100;
+        if (score > 99) {
+            System.out.print("You got the high socre!");
+            score = 0;
+        }
+    }
+}
